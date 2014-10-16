133b25639416f1cd60e976d7a523e9e5817bf804
Young <admin@appledeMacBook-Air.local>
2014-10-16 14:48:29 +0800

    add Button

---
+++ project01/Buttonasdw.h
@@ -0,0 +1,13 @@
+//
+//  Buttonasdw.h
+//  project01
+//
+//  Created by Young on 14-10-16.
+//  Copyright (c) 2014年 com.young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Buttonasdw : UIButton
+
+@end
---
+++ project01/Buttonasdw.m
@@ -0,0 +1,21 @@
+//
+//  Buttonasdw.m
+//  project01
+//
+//  Created by Young on 14-10-16.
+//  Copyright (c) 2014年 com.young. All rights reserved.
+//
+
+#import "Buttonasdw.h"
+
+@implementation Buttonasdw
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
