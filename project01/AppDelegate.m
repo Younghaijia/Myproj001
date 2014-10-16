4ed3cc933a1400a0defbb8b955577e083938e125
Young <admin@appledeMacBook-Air.local>
2014-10-16 14:34:00 +0800

    test commit 001

--- project01/AppDelegate.m
+++ project01/AppDelegate.m
@@ -14,6 +14,10 @@
 
 @implementation AppDelegate
 
+- (void)test
+{
+    
+}
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
