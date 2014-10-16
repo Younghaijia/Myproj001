5bc4ad607bb21544530cb833f2c0abc739db7c55
Young <admin@appledeMacBook-Air.local>
2014-10-16 14:28:11 +0800

    Initial Commit

---
+++ project01/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  project01
+//
+//  Created by Young on 14-10-16.
+//  Copyright (c) 2014年 com.young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ project01/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  project01
+//
+//  Created by Young on 14-10-16.
+//  Copyright (c) 2014年 com.young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
